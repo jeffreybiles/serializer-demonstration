c4a32ad3e9d4a9adedee09aeb7e42f14aaac514e
jeffreybiles <bilesjeffrey@gmail.com>
2016-04-15 17:29:07 -0500

    two methods for fixing the id: normalize and extractId

--- app/serializers/quote.js
+++ app/serializers/quote.js
@@ -1,4 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+  // You can solve this with normalize
+  // normalize is for the *individual* JSON API objects returned
+
+  // normalize(modelClass, resourceHash){
+  //   resourceHash.id = resourceHash.id.replace('quote_', '')
+  //   return this._super(...arguments)
+  // }
+
+  // You can also solve it more efficiently with extractId
+  extractId(modelClass, resourceHash) {
+    return resourceHash.id.replace('quote_', '')
+  }
 });
